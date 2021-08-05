9a52886b25c8e591cc89572f99e6af140d55e821
tinle15 <tinle1596@gmail.com>
2021-08-05 11:39:56 -0700

    Add navigation bar and drawer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <div id="app">
+    <navbar></navbar>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import navbar from './components/nagivation/navbar.vue'
+
+
+export default {
+  name: "Home",
+  components: {
+    navbar
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/nagivation/navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <v-app-bar
+      absolute
+      color="indigo darken-2"
+      dense
+      hide-on-scroll
+      scroll-target="#scrolling-techniques"
+    >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-app-bar-title>Doan Emmanuel</v-app-bar-title>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense> 
+          <v-list-item-group v-model="group">
+              <v-list-item>
+                  <v-list-item-icon>
+                      <v-icon>mdi-home</v-icon>                      
+                  </v-list-item-icon>
+                  <v-list-item-title>Home</v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                  <v-list-item-icon>
+                      <v-icon>mdi-information</v-icon>                      
+                  </v-list-item-icon>
+                  <v-list-item-title>About</v-list-item-title>
+              </v-list-item>
+          </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-sheet id="scrolling-techniques" class="overflow-y-auto" max-height="600">
+      <v-container style="height: 1000px"></v-container>
+    </v-sheet>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+};
+</script>
+
+<style>
+</style>
