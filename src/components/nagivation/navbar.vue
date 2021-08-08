59bcc984eefb4f5747ac149a3a56a0a946812636
Tin Le <tinle1596@gmail.com>
2021-08-07 18:34:51 -0700

    Refactored routing

--- src/components/nagivation/navbar.vue
+++ src/components/nagivation/navbar.vue
@@ -2,29 +2,25 @@
   <div id="nav">
     <v-app-bar fixed dense hide-on-scroll>
       <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-      <v-app-bar-title>Doan Emmanuel</v-app-bar-title>      
+      <v-app-bar-title>Doan Emmanuel</v-app-bar-title>
     </v-app-bar>
     <v-navigation-drawer v-model="drawer" absolute app temporary>
       <v-list nav dense>
-        <v-list-item-group v-model="group">
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-information</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>About</v-list-item-title>
+        <v-list-item-group v-model="link">
+          <v-list-item v-for="link in links" :key="link.icon" :to="link.route">
+            <v-list-item-avatar>
+              <v-icon v-text="link.icon"></v-icon>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title v-text="link.text"></v-list-item-title>
+            </v-list-item-content>
           </v-list-item>
         </v-list-item-group>
       </v-list>
       <template v-slot:append>
         <div class="pa-2">
           <v-card elevation="0" class="d-flex justify-space-between">
-            <div class="pa-2 align-self-center">Tieng Viet</div>
+            <div class="pa-2 align-self-center">Tiếng Việt</div>
             <v-switch class="pa-2" @click="toggleTranslation()"></v-switch>
           </v-card>
         </div>
@@ -34,18 +30,30 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions } from "vuex";
 
 export default {
   data: () => ({
     drawer: false,
-    group: null,
+    link: 1,
+    links: [
+      {
+        icon: "mdi-home",
+        text: "Home",
+        route: "/"
+      },
+      {
+        icon: "mdi-information",
+        text: "About",
+        route: "/about"
+      }
+    ]
   }),
-  methods:{
+  methods: {
     ...mapActions({
-      toggleTranslation: 'toggleTranslation'
+      toggleTranslation: "toggleTranslation"
     })
-  }  
+  }
 };
 </script>
 
