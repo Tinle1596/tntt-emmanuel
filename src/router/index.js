c8a22c241dafc0496d71eddf6831c1e9ad2a4876
tinle15 <tinle1596@gmail.com>
2021-10-31 11:39:51 -0700

    add auth requirement for leaders page

--- src/router/index.js
+++ src/router/index.js
@@ -55,7 +55,8 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "leaders" */ '../views/Leaders.vue')
+    component: () => import(/* webpackChunkName: "leaders" */ '../views/Leaders.vue'),
+    meta: {requiresAuth: true}
   }
 ]
 
